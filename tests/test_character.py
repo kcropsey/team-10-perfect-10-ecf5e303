a13ae9480a1c4031058be030f3fd46375016d335
Jeremy <jeremy@>
2023-06-15 21:16:23 +0000

    Refactor Map test double

--- tests/test_character.py
+++ tests/test_character.py
@@ -1,6 +1,12 @@
 from unittest import TestCase
 from levelup.character import Character
 
+class TestMap():
+    starting_position = {}
+
+    def calculatePosition(position, direction):
+        return position
+
 class TestCharacterInitWithName(TestCase):
     def test_init(self):
         ARBITRARY_NAME = "MyName"
@@ -12,10 +18,7 @@ class TestCharacterInitWithName(TestCase):
         self.assertEqual(testobj.default_name, testobj.name)
 
     def test_enter_map(self):
-        class Map():
-            starting_position = {}
-
-        map = Map();
+        map = TestMap()
 
         testobj = Character()
         testobj.enter_map(map)
