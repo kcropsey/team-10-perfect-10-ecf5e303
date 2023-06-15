dfcd27a5d941f5df03059f7e1192191e19a801ab
Jeremy <jeremy@>
2023-06-15 22:20:10 +0000

    Character move

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -6,7 +6,12 @@ class Character:
         if character_name is None:
             character_name = self.default_name
         self.name = character_name
+        self.move_count = 0
 
     def enter_map(self, map):
         self.map = map
         self.position = map.starting_position
+    
+    def move(self, direction):
+        self.move_count = self.move_count + 1
+        self.position = self.map.calculatePosition(self.position, direction) 
