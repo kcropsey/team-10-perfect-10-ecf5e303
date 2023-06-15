6d231cbd1cacc48f0027ff7996df1c65bdd1a211
Jeremy <jeremy@>
2023-06-15 21:18:06 +0000

    Character move test

--- tests/test_character.py
+++ tests/test_character.py
@@ -3,8 +3,10 @@ from levelup.character import Character
 
 class TestMap():
     starting_position = {}
+    has_calculated_position = False
 
-    def calculatePosition(position, direction):
+    def calculatePosition(self, position, direction):
+        self.has_calculated_position = True
         return position
 
 class TestCharacterInitWithName(TestCase):
@@ -25,3 +27,17 @@ class TestCharacterInitWithName(TestCase):
         self.assertEqual(testobj.map, map)
         self.assertEqual(testobj.position, map.starting_position)
 
+    def test_move(self):
+        map = TestMap()
+
+        testobj = Character()
+        testobj.enter_map(map)
+        start_move_count = testobj.move_count
+        start_position = testobj.position
+        testobj.move("NORTH")
+        # Move count incremented
+        self.assertEqual(start_move_count + 1, testobj.move_count)
+        # The position is still a position
+        self.assertEqual(type(start_position), type(testobj.position))
+        # TestMap has been called
+        self.assertTrue(map.has_calculated_position)
